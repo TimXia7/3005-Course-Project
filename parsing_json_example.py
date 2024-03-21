290d974bb1c520daa129968ce0d50b4e046160bb
AquaShotRyan <ryantran574@gmail.com>
2024-03-21 15:51:47 -0400

    make a function for getting all match ids from a season json

--- parsing_json_example.py
+++ parsing_json_example.py
@@ -1,7 +1,10 @@
 import json
 
+
+
 #ryan's branch test
 def main():
     with open("./3005-Course-Project/json/90.json", "r", encoding="utf-8") as read_file:
         data = json.load(read_file)
     print(print(json.dumps(data[0], indent=4)))
+    
